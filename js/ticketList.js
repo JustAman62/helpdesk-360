4fccce91da3878eb5930ac7c7c22b17dc2ae1be4
BrownKnight <amanrk@hotmail.co.uk>
2017-10-31 16:44:28 +0000

    split ticket generation into its own js file, to load only on ticket list page

---
+++ js/ticketList.js
@@ -0,0 +1,24 @@
+var openTicket =  `<a href="#" class="open-ticket-clickable">
+    <div class="w-100 open-ticket-clickable">
+    <div class="d-flex ticket-header">
+    <h4 class="card-title">Ticket 2</h4>
+</div>
+<div class="ticket-body">
+    <p class="card-text">Ticket Description</p>
+
+<!--Table footer-->
+<table class="w-100">
+    <tr>
+    <td>Date Created</td>
+<td class="text-right">Unassigned / Specialist</td>
+    </tr>
+    </table>
+    </div>
+    </div>
+    </a>`
+
+function addTicket() {
+    var html = $('#open-ticket-list').html();
+    $('#open-ticket-list').html(html + openTicket);
+    console.log(openTicket, 'lala');
+}
