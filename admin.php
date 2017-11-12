a387ee1cacc5b3d708c84fd547ca064478d61bf8
BrownKnight <amanrk@hotmail.co.uk>
2017-11-12 18:13:27 +0000

    adjust heading

--- admin.php
+++ admin.php
@@ -58,7 +58,7 @@ if (!isset($_SESSION['user'])) {
 <div class="container mt-4">
     <div class="row">
         <div class="col-12">
-            <h1 class="display-4 text-center">Analytics</h1>
+            <h1 class="display-4 text-center">Admin</h1>
         </div>
     </div>
 </div>
