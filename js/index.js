78d045dd4c58054e5f8b6942e783262767ec0993
BrownKnight <amanrk@hotmail.co.uk>
2017-11-12 16:01:55 +0000

    fix ticket sorting by generating a proper copy of the tickets array

--- js/index.js
+++ js/index.js
@@ -7,7 +7,8 @@ function navToTicket() {
 $(function() {
 //    When the page is loaded, display the 5 most recent tickets
 //    To do this, sort tickets by dateCreated
-    let sortedTickets = tickets.sort(function(a, b) {
+    let sortedTickets = tickets.slice(0);
+    sortedTickets.sort(function(a, b) {
         let aString = a.dateCreated.split('/');
         let aDay = aString[0];
         let aMonth = aString[1];
@@ -18,11 +19,11 @@ $(function() {
         let bDay = bString[0];
         let bMonth = bString[1];
         let bYear = bString[2];
-        let bDate = new Date(bYear, bMonth-1, bDay)
+        let bDate = new Date(bYear, bMonth-1, bDay);
 
         return bDate - aDate;
     });
     for (let i = 0; i < 3; i++) {
-        addOpenTicket(i);
+        addOpenTicket(sortedTickets[i].ticketNumber-1);
     }
 });
