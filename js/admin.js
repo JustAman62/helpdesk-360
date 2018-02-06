f5abd9da0014f4e1f63c59d0f80a7c8e5ee4119a
BrownKnight <amanrk@hotmail.co.uk>
2018-02-06 21:29:40 +0000

    Add a New User button to end of list of users

--- js/admin.js
+++ js/admin.js
@@ -4,17 +4,30 @@ $(function(){
 
 function loadUserList() {
     $.get('././scripts/getFullUserDetails.php', function(result) {
+        //Create list items for each user returned in the query
         for (let i in result) {
             createUserItem(result[i]);
         }
+
+        //Create a list item to allow a new user to be added
+        let addUser = document.createElement('button');
+        addUser.setAttribute('class', 'list-group-item list-group-item-action');
+        let plusSign = document.createElement('i');
+        plusSign.setAttribute('class', 'icon icon-plus-circled');
+        addUser.append(plusSign);
+        addUser.append(document.createTextNode(' Add User'));
+
+        $('#user-list').append(addUser);
+
+        //Update the footer to show how many users are in the list
         $('#user-count').text(result.length);
-        console.log(result);
+
     }, 'json');
 }
 
 function createUserItem(user) {
     let item = document.createElement('button');
     item.setAttribute('class', 'list-group-item list-group-item-action');
-    item.append(document.createTextNode(user.userID));
+    item.append(document.createTextNode(user.firstName + ' ' + user.lastName));
     $('#user-list').append(item);
 }
