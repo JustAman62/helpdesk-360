59d2fda318869d90cb04add7f6ea056924972984
BrownKnight <amanrk@hotmail.co.uk>
2018-02-06 15:47:01 +0000

    dynamic ticket generation - complete dynamic generation using dummy data

--- js/ticketGenerator.js
+++ js/ticketGenerator.js
@@ -1,8 +1,31 @@
-function createTicket(ticketNum) {
-    let ticket = tickets[ticketNum];
+`        <a class="col-12 col-md-6 col-xl-4 p-3 ticket" href="ticket.php?ticketNum=1">
+            <div class="ticket-header justify-content-between">
+                <h5>Heading</h5>
+                <h5><span class="badge badge-primary">Low Priority</span></h5>
+            </div>
+            <div class="ticket-body">
+                <div class="ticket-description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque ea eaque earum enim esse hic, inventore ipsam magnam necessitatibus, </p>
+                </div>
+                <div class="ticket-footer justify-content-between">
+                    <span>left</span>
+                    <span>right</span>
+                </div>
+            </div>
+        </a>`
 
-    let ticketContainer = createNewElement('a', 'col-12 col-md-6 col-xl-4 p-3 ticket')
+
+function createTicket(ticketNum) {
+    let ticket = tickets.find(function(element) {
+        return element.ticketNumber === ticketNum;
+    });
+
+    console.log(ticket);
+
+    let ticketContainer = createNewElement('a', 'col-12 col-md-6 col-xl-4 p-3 ticket');
     ticketContainer.setAttribute('href', 'ticket.php?ticketNum=' + ticket.ticketNumber);
+
+    //--------
 
     let ticketHeaderContainer = createNewElement('div', 'ticket-header justify-content-between');
 
@@ -21,8 +44,29 @@ function createTicket(ticketNum) {
 
     //---------
 
+    let ticketBody = createNewElement('div', 'ticket-body');
 
+    let ticketDescriptionContainer = createNewElement('div', 'ticket-description');
+    let ticketDescription = createNewElement('p');
+    ticketDescription.append(document.createTextNode(ticket.originalDescription));
+    ticketDescriptionContainer.append(ticketDescription);
+    ticketBody.append(ticketDescriptionContainer);
 
+    let ticketFooter = createNewElement('div', 'ticket-footer justify-content-between');
+    let dateCreated = createNewElement('span', '');
+    dateCreated.append(document.createTextNode('Date'));
+    ticketFooter.append(dateCreated);
+    let assignedSpecialist = createNewElement('span', '');
+    assignedSpecialist.append(document.createTextNode('Not Assigned'));
+    ticketFooter.append(assignedSpecialist);
+
+    ticketBody.append(ticketFooter);
+
+    ticketContainer.append(ticketBody);
+
+    //---------
+
+    $('#ticket-list').append(ticketContainer);
 
 
 }
--- js/ticketList.js
+++ js/ticketList.js
@@ -5,23 +5,6 @@ $(function() {
 
 function showTickets(){
 
-    let check = document.getElementById("CTicket").checked;
-    console.log(check);
-
-    if(check===true)
-    {
-        $("#closed-ticket-list").show();
-        for(let i in tickets) {
-
-            if(tickets[i].ticketStatus===0)
-            addClosedTicket(i);
-        }
-    }
-    if(check === false)
-    {
-        $("#closed-ticket-list").hide();
-    }
-
     for (let i in tickets) {
         if (tickets[i].ticketStatus===1) createTicket(tickets[i].ticketNumber);
     }
