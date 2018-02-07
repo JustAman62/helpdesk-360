d2c640e2ceddf98e70f514967472558713750fd6
jessm12 <jessmccreery@hotmail.co.uk>
2018-02-07 15:18:49 +0000

    Login

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
   if (!isset($_SESSION['user'])) {
       header('Location: login.php');
   }
+
+  require('connect.php');
+
+  $sql = "SELECT * FROM Users WHERE employeeid='".$_REQUEST["user"]. "' and password = '". $_REQUEST["password"]."'");
+
+  $result = $conn->query($sql);
+
+  if ($conn->error) die($conn->error);
+
+  echo json_encode($result->fetch_object());
 ?>
 
 <!DOCTYPE html>
