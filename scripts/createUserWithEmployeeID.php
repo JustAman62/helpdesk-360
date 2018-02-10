775abf6cc914f6719956500d56e2d6a773845518
BrownKnight <amanrk@hotmail.co.uk>
2018-02-10 15:50:29 +0000

    ensure duplicate user accounts are never created

--- scripts/createUserWithEmployeeID.php
+++ scripts/createUserWithEmployeeID.php
@@ -12,13 +12,23 @@ require('connect.php');
 
 $employeeID = $_REQUEST['employeeid'];
 
-// Find the max user id
+// Find the max user id, and add one to get the value of the next ID
 
 $sql = "SELECT MAX(userID) FROM Users";
 
 $userID = $conn->query($sql);
 if ($conn->error) die($conn->error);
 $userID = $userID->fetch_row()[0]+1;
+
+// Check that the employee doesn't already have a user account
+
+$sql = "SELECT * FROM Users WHERE employeeID = $employeeID";
+$numUsers = $conn->query($sql);
+if ($conn->error) die($conn->error);
+if ($numUsers->num_rows > 0) die('Employee already has user account');
+
+//Create the user account for the user
+
 $sql = "INSERT INTO Users (userID, password, accessLevel, employeeID) VALUES ($userID, 'password', 1, $employeeID)";
 
 $result = $conn->query($sql);
