34efc91deadafe8a9992fb923b76be0fd6cf4104
BrownKnight <amanrk@hotmail.co.uk>
2018-02-12 16:18:20 +0000

    implement username storing on SESSION

--- scripts/userLogin.php
+++ scripts/userLogin.php
@@ -6,7 +6,7 @@ require('connect.php');
 $userID = $_REQUEST['userid'];
 $password = $_REQUEST['password'];
 
-$sql = "SELECT * FROM Users WHERE userID = '$userID' AND password = '$password'";
+$sql = "SELECT * FROM Users LEFT JOIN Employees ON Users.employeeID = Employees.employeeID WHERE userID = '$userID' AND password = '$password'";
 
 $result = $conn->query($sql);
 if ($conn->error) die($conn->error);
@@ -17,4 +17,8 @@ if ($result->num_rows == 0) {
 else {
     echo 'success';
     $_SESSION['userid'] = $userID;
+    $employee = $result->fetch_object();
+    $_SESSION['username'] = $employee->firstName." ".$employee->lastName;
 }
+
+
