f9d00fb9b5d5f5b6fb25e0883cf9ecc0333875d6
BrownKnight <amanrk@hotmail.co.uk>
2018-02-13 15:31:40 +0000

    convert ticket data retrieval to rely on database

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(function() {
 //    When the page is loaded, display the 5 most recent tickets
 //    To do this, sort tickets by dateCreated
-
+    $.get('scripts/getTickets.php', {sort:'dateCreated'}, function(result) {
+        console.log(result);
+    }, 'json');
 });
