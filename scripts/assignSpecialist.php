90d7160981c8945fec0554b7f917b5870535cbf6
Linus Kurz <linuskurz21@gmail.com>
2018-02-17 21:03:57 +0000

    sql

--- scripts/assignSpecialist.php
+++ scripts/assignSpecialist.php
@@ -10,7 +10,7 @@ if ($conn->error) die ($conn->error);
 
 $problemtypeid = $result->fetch_row()[0];
 
-$sql = "SELECT Tbl1.userID, Tbl1.Problems
+$sql = "SELECT Employees.firstName, Employees.lastName, Tbl1.userID, Tbl1.Problems
         FROM
         (SELECT Tbl.userID, MAX(Tbl.Count) AS Problems
         FROM (SELECT Users.userID, COUNT(1) as Count
@@ -21,8 +21,9 @@ $sql = "SELECT Tbl1.userID, Tbl1.Problems
         UNION
         SELECT Specialists.userID, 0 AS Count
         FROM Specialists) AS Tbl, Specialists, Users
-        GROUP BY Tbl.userID) AS Tbl1, Specialists
+        GROUP BY Tbl.userID) AS Tbl1, Specialists, Employees
         WHERE Tbl1.userID = Specialists.userID
+        AND Employees.employeeID = Specialists.userID
         AND Specialists.problemTypeID = 1008
         ORDER BY Problems";
 
