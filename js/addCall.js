ea400da3af80cfbd2e611437eff3e954be117a18
Linus Kurz <linuskurz21@gmail.com>
2018-02-17 20:35:15 +0000

    .userID

--- js/addCall.js
+++ js/addCall.js
@@ -77,7 +77,7 @@ $(function() {
        console.log("hey");
        let array1 = [];
        for (let i in result) {
-         var result1 = JSON.stringify(result[i][0]);
+         var result1 = JSON.stringify(result[i].userID);
          array1.push(result1);
        }
        specialistList.list = array1;
