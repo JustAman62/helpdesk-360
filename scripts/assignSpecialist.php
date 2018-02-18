ad2b1170377bc674acb15df1a2463a7182dea5c7
Marinela18 <rocamarinela@hotmail.com>
2018-02-18 09:28:43 +0000

    Show all specialists

--- scripts/assignSpecialist.php
+++ scripts/assignSpecialist.php
@@ -2,18 +2,18 @@
 
 require 'connect.php';
 
-include 'findProblemTypeName.php';
+/*include 'findProblemTypeName.php';
 
 $problemtype = $_REQUEST['problemtype'];
 $sql = "SELECT problemTypeID FROM ProblemTypes WHERE problemTypeName = '$problemtype'";
 $result = $conn->query($sql);
 if ($conn->error) die ($conn->error);
 
-$problemtypeid = $result->fetch_row()[0];
+$problemtypeid = $result->fetch_row()[0];*/
 
 //$problemtypeid = JSON.stringify($rows);
 
-$sql = "SELECT Employees.firstName, Employees.lastName, Tbl1.userID, Tbl1.Problems
+/*$sql = "SELECT Employees.firstName, Employees.lastName, Tbl1.userID, Tbl1.Problems
         FROM
         (SELECT Tbl.userID, MAX(Tbl.Count) AS Problems
         FROM (SELECT Users.userID, COUNT(1) as Count
@@ -28,6 +28,22 @@ $sql = "SELECT Employees.firstName, Employees.lastName, Tbl1.userID, Tbl1.Proble
         WHERE Tbl1.userID = Specialists.userID
         AND Employees.employeeID = Specialists.userID
         AND Specialists.problemTypeID = $problemtypeid
+        ORDER BY Problems";*/
+
+$sql = "SELECT DISTINCT Employees.firstName, Employees.lastName, Tbl1.userID, Tbl1.Problems
+        FROM
+        (SELECT Tbl.userID, MAX(Tbl.Count) AS Problems
+        FROM (SELECT Users.userID, COUNT(1) as Count
+        FROM Tickets, Specialists, Users
+        WHERE Specialists.specialistID = Tickets.specialistID
+        AND Users.userID = Specialists.userID
+        GROUP BY Users.userID
+        UNION
+        SELECT Specialists.userID, 0 AS Count
+        FROM Specialists) AS Tbl, Specialists, Users
+        GROUP BY Tbl.userID) AS Tbl1, Specialists, Employees
+        WHERE Tbl1.userID = Specialists.userID
+        AND Employees.employeeID = Specialists.userID
         ORDER BY Problems";
 
 $result = $conn->query($sql);
