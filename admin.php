fa6ca0b73cd1685308fddb05f594ae74c4ce4607
jessm12 <jessmccreery@hotmail.co.uk>
2018-02-18 22:37:22 +0000

    test

--- admin.php
+++ admin.php
@@ -83,18 +83,16 @@ if (!isset($_SESSION['userid'])) {
                     <span id="software-count"></span> Software
                 </div>
             </div>
-          </div>
-          <div class="col-md-4 col-12">
-              <div class="card">
-                  <h5 class="card-header text-center">
-                      Hardware
-                      <a class="text-secondary" href="#"><i class="icon icon-search"></i></a>
-                  </h5>
-                  <div class="list-group list-group-flush" id="hardware-list"></div>
-                  <div class="card-footer text-muted small text-center">
-                      <span id="hardware-count"></span> Hardware
-                  </div>
-              </div>
+            <div class="card">
+                <h5 class="card-header text-center">
+                    Hardware
+                    <a class="text-secondary" href="#"><i class="icon icon-search"></i></a>
+                </h5>
+                <div class="list-group list-group-flush" id="hardware-list"></div>
+                <div class="card-footer text-muted small text-center">
+                    <span id="hardware-count"></span> Hardware
+                </div>
+            </div>
           </div>
         </div>
     </div>
