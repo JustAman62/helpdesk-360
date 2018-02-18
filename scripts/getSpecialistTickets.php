3d54258a34d773738b350946c9aa382abd3f2391
jessm12 <jessmccreery@hotmail.co.uk>
2018-02-18 16:32:22 +0000

    test

--- scripts/getSpecialistTickets.php
+++ scripts/getSpecialistTickets.php
@@ -2,7 +2,9 @@
 
 require 'connect.php';
 
-$userid = $_SESSION['userid'];
+if (!isset($_SESSION['userid'])) {
+    $userid = $_SESSION['userid'];
+}
 console.log($userid);
 
 $sql = "SELECT Tickets.ticketNumber, Tickets.userID, Tickets.employeeID, Tickets.dateCreated, Tickets.dateClosed, Tickets.priority, Tickets.problemTypeID, Tickets.originalDescription, Tickets.specialistID, Tickets.ticketStatus, Tickets.serialNumber, Tickets.licenceNumber, Tickets.operatingSystem
