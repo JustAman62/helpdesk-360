3afe10cceceb83b2e2c01b08c0cecdb42a7e7b93
jessm12 <jessmccreery@hotmail.co.uk>
2018-02-19 10:19:36 +0000

    software

--- scripts/createSoftware.php
+++ scripts/createSoftware.php
@@ -2,8 +2,12 @@
 
 require('connect.php');
 
+$licencenumber = $_REQUEST['licencenumber'];
+$name = $_REQUEST['name'];
+$type = $_REQUEST['name'];
+
 //Create the software entry in database
 
-$sql = "INSERT INTO `Software`(`licenceNumber`, `name`, `type`) VALUES ($licencenumber, $name, $type)";
+$sql = "INSERT INTO `Software`(`licenceNumber`, `type`, `name`) VALUES ('$licencenumber', '$type', '$name')";
 $result = $conn->query($sql);
 if ($conn->error) die($conn->error);
