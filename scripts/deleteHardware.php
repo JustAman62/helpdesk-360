acd7616e8c26bc70f0519a6aad04c7bb119daa18
jessm12 <jessmccreery@hotmail.co.uk>
2018-02-19 13:47:23 +0000

    delete

--- scripts/deleteHardware.php
+++ scripts/deleteHardware.php
@@ -6,3 +6,6 @@ require('connect.php');
 $serialnumber = $_REQUEST['serialnumber'];
 
 $sql = "DELETE FROM `Equipment` WHERE serialNumber = $serialnumber";
+
+$result = $conn->query($sql);
+if ($conn->error) die($conn->error);
