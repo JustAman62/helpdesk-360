8b1fc8017da7b3090bd0c7d58931468fd8f9f7e3
Linus Kurz <linuskurz21@gmail.com>
2018-02-19 02:08:31 +0000

    test

--- js/addCall.js
+++ js/addCall.js
@@ -111,18 +111,17 @@ function assignNewSpecialist(){
 function assignedSpecialistName(){
     let input = $('#available-Specialists');
     let specialistName = new Awesomplete(input[0]);
-    console.log("hi");
-    let variable = '#available-Specialists'.val().firstName.concat(" ", '#available-Specialists'.val().lastName);
-    // $.get('#available-Specialists', {specialistname: $('#available-Specialists').val()}, function(result) {
-    //     console.log("hello");
-    //     let variable = "";
-    //     for (let i in '#available-Specialists'.val()) {
-    //       console.log("hii");
-    //         var result1 = '#available-Specialists'[i].firstName.concat(" ", '#available-Specialists'[i].lastName);
-    //         variable.push(result1);
-    //     }
-    //     specialistName.list = variable;
-    // }, 'json');
+    // let variable = '#available-Specialists'.val().firstName.concat(" ", '#available-Specialists'.val().lastName);
+    $.get('#available-Specialists', {specialistname: $('#available-Specialists')}, function(result) {
+        console.log("hello");
+        let variable = "";
+        for (let i in '#available-Specialists'.val()) {
+          console.log("hii");
+            var result1 = '#available-Specialists'[i].firstName.concat(" ", '#available-Specialists'[i].lastName);
+            variable.push(result1);
+        }
+        specialistName.list = variable;
+    }, 'json');
     input.on('focus', function(){
         specialistName.evaluate();
         specialistName.open();
