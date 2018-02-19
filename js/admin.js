ff665fc379efc5af240b737713d23c1d55464b2a
jessm12 <jessmccreery@hotmail.co.uk>
2018-02-19 01:09:39 +0000

    test

--- js/admin.js
+++ js/admin.js
@@ -17,6 +17,7 @@ $(function(){
             licenceNumber = event.relatedTarget.dataset.licenceNumber;
             loadSoftwareByLicence(licenceNumber)
         }
+        console.log(licenceNumber);
     });
 });
 
