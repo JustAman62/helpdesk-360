289aa157cb5b5c6226c9b9b4a961d5fddb0f24b2
Linus Kurz <linuskurz21@gmail.com>
2018-02-19 00:36:43 +0000

    test

--- addCall.php
+++ addCall.php
@@ -134,7 +134,6 @@ if (!isset($_SESSION['userid'])) {
                         <div class="col-12 col-md-4" style="z-index: 100">
                             <label for="assigned-specialist-name">Assigned Specialist</label>
                             <input type="text" class="form-control" id="assigned-specialist-name" placeholder="John Doe" readonly>
-
                         </div>
                     </div>
                     <div class="row mt-4">
